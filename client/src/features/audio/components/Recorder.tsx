cbc69e15163edeff1b20bb0a3fce5ea6b9563d24
chethtrayen <chethtrayen1991@gmail.com>
2023-02-22 02:12:28 -0500

    Add spacing

--- client/src/features/audio/components/Recorder.tsx
+++ client/src/features/audio/components/Recorder.tsx
@@ -15,7 +15,7 @@ interface RecorderProps {
 
 const Recorder = ({ audioSrc, setAudioSrc, startRecording, status, stopRecording }: RecorderProps) => {
   return (
-    <div className="flex flex-row" style={{ gap: "10px" }}>
+    <div className="flex flex-row space-x-5">
       <DeleteBtn setAudioSrc={setAudioSrc} audioSrc={audioSrc} />
 
       <RecordingBtn startRecording={startRecording} status={status} stopRecording={stopRecording} />
